6bcbe8bfc4ceddee8cb051c797ae2e2be8e4fb8d
Frederik Hjorth <frederikhjorth@gmail.com>
2016-02-12 17:29:08 +0100

    start writing revision notes

--- analysis/parlbias_analysis.R
+++ analysis/parlbias_analysis.R
@@ -28,14 +28,33 @@ ggplot(subset(ft,chair==0),aes(x=secs)) + geom_density()
 ggplot(ft,aes(x=timeofday,y=secs)) + geom_point() + facet_grid(debate~.)
 
 
+#get list of chairmen by debate
+allchairmen<-ftall %>%
+  filter(chair==1) %>%
+  group_by(chairname,debate) %>%
+  summarise(remarks=length(chairname))
+
+seatvec<-c(16,rep(22,2),rep(47,7),rep(8,3),rep(18,2),47,23,16,23,16,9,47,44,34,17,17,13,45,rep(47,3),45,rep(47,4),rep(22,4),37,25,22,25,22,16,14,rep(46,2)) #careful! here I manually type in the seats for each chairman's party by debate
+allchairmen$seats<-seatvec
+
+allchairmen<-allchairmen %>%
+  group_by(chairname) %>%
+  summarise(remarks=sum(remarks),debates=length(debate),avgseats=mean(seats)) %>%
+  mutate(president=ifelse(chairname %in% c("Thor Pedersen","Mogens Lykketoft","Pia Kjaersgaard"),1,0))
+
+#can we predict chairman activity?
+summary(remarksm1<-lm(remarks~president+debates,data=allchairmen))
+summary(remarksm2<-lm(remarks~president+debates+avgseats,data=allchairmen))
+summary(remarksm3<-lm(remarks~president+avgseats,data=allchairmen))
+
 ### REGRESSION MODELS
 
 #define basic models
 m1<-lm(m1f<-as.formula(secs~copartisan),data=ft)
-m2<-lm(m2f<-as.formula(secs~copartisan+timeofday+female),data=ft)
-m3<-lm(m3f<-as.formula(secs~copartisan+timeofday+female+factor(chairparty)),data=ft)
-m4<-lm(m4f<-as.formula(secs~copartisan+timeofday+female+debate),data=ft)
-m5<-lm(m5f<-as.formula(secs~copartisan+timeofday+female+debate+factor(chairparty)+factor(coarseparty)),data=ft)
+m2<-lm(m2f<-as.formula(secs~copartisan+timeofday),data=ft)
+m3<-lm(m3f<-as.formula(secs~copartisan+timeofday+female+debatetype),data=ft)
+m4<-lm(m4f<-as.formula(secs~copartisan+timeofday+female+debatetype+factor(coarseparty)),data=ft)
+m5<-lm(m5f<-as.formula(secs~copartisan+timeofday+female+debatetype+factor(coarseparty)+factor(chairparty)),data=ft)
 
 stargazer(m1,m2,m3,m4,m5,type="text",omit=c("factor","debate"),omit.stat=c("f","ser"))
 stargazer(m1,m2,m3,m4,m5,type="text",omit=c("debate"),omit.stat=c("f","ser"))
@@ -67,15 +86,16 @@ lengths<-ft %>%
   filter(speeches>0)
 
 meanspeechesperchair<-mean(lengths$speeches)
+maxspeechesperchair<-max(lengths$speeches)
 
-#how many minutes does 2.8 seconds add up to over the average debate per chair?
-effectests$est[4]*meanspeechesperchair/60
+#how many seconds does 2.8 seconds add up to over the average debate per chair?
+effectests$est[4]*maxspeechesperchair
 
 summary(m1.logit<-glm(secsgt60~copartisan,data=ft,family=binomial()))
-summary(m2.logit<-glm(secsgt60~copartisan+timeofday+female,data=ft,family=binomial()))
-summary(m3.logit<-glm(secsgt60~copartisan+timeofday+female+factor(chairparty)+factor(coarseparty),data=ft,family=binomial()))
-summary(m4.logit<-glm(secsgt60~copartisan+timeofday+female+debate,data=ft,family=binomial()))
-summary(m5.logit<-glm(secsgt60~copartisan+timeofday+female+debate+factor(chairparty)+factor(coarseparty),data=ft,family=binomial()))
+summary(m2.logit<-glm(secsgt60~copartisan+timeofday,data=ft,family=binomial()))
+summary(m3.logit<-glm(secsgt60~copartisan+timeofday+female+debatetype,data=ft,family=binomial()))
+summary(m4.logit<-glm(secsgt60~copartisan+timeofday+female+debatetype+factor(coarseparty),data=ft,family=binomial()))
+summary(m5.logit<-glm(secsgt60~copartisan+timeofday+female+debatetype+factor(coarseparty)+factor(chairparty),data=ft,family=binomial()))
 
 require(mfx)
 effectests[6,1:2]<-logitmfx(m1.logit$formula,data=ft)$mfxest[1,1:2]*100
@@ -121,35 +141,63 @@ chairranefs<-ft %>%
   mutate(chairorder=1:20,chairparty=paste(chair," ","(",party,")",sep=""))
 
 
+
+#robustness check 1
+require(rms)
+m1robrs<-robcov(ols(m1f,data=subset(ft,leadshipparty==1),x=T,y=T),cluster=subset(ft,leadshipparty==1)$chairname)
+m2robrs<-robcov(ols(m2f,data=subset(ft,leadshipparty==1),x=T,y=T),cluster=subset(ft,leadshipparty==1)$chairname)
+m3robrs<-robcov(ols(m3f,data=subset(ft,leadshipparty==1),x=T,y=T),cluster=subset(ft,leadshipparty==1)$chairname)
+m4robrs<-robcov(ols(m4f,data=subset(ft,leadshipparty==1),x=T,y=T),cluster=subset(ft,leadshipparty==1)$chairname)
+m5robrs<-robcov(ols(m5f,data=subset(ft,leadshipparty==1),x=T,y=T),cluster=subset(ft,leadshipparty==1)$chairname)
+
+#robustness check 2
+m1fdfe<-as.formula(secs~copartisan+debate)
+m2fdfe<-as.formula(secs~copartisan+timeofday+debate)
+m3fdfe<-as.formula(secs~copartisan+timeofday+female+debate)
+m4fdfe<-as.formula(secs~copartisan+timeofday+female+factor(coarseparty)+debate)
+m5fdfe<-as.formula(secs~copartisan+timeofday+female+factor(coarseparty)+factor(chairparty)+debate)
+
+m1robdfe<-robcov(ols(m1fdfe,data=ft,x=T,y=T),cluster=ft$chairname)
+m2robdfe<-robcov(ols(m2fdfe,data=ft,x=T,y=T),cluster=ft$chairname)
+m3robdfe<-robcov(ols(m3fdfe,data=ft,x=T,y=T),cluster=ft$chairname)
+m4robdfe<-robcov(ols(m4fdfe,data=ft,x=T,y=T),cluster=ft$chairname)
+m5robdfe<-robcov(ols(m5fdfe,data=ft,x=T,y=T),cluster=ft$chairname)
+
+
 ### TABLES
 
-checkmarks<-c("Speaker party fixed effects & & & $\\checkmark$ & & $\\checkmark$ \\\\", "Chair party fixed effects & & & $\\checkmark$ & & $\\checkmark$ \\\\", "Debate fixed effects & & & & $\\checkmark$ & $\\checkmark$ \\\\")
-covarlabs<-c("Copartisan","Time of day","Gender (female)","Intercept")
+checkmarks<-c("Speaker party FE & & & $\\checkmark$ & & $\\checkmark$ \\\\", "Chair party FE & & & & & $\\checkmark$ \\\\")
+#, "Debate FE & & & & $\\checkmark$ & $\\checkmark$ \\\\")
+covarlabs<-c("Copartisan","Time of day","Gender (female)","Debate type (Opening)","Intercept")
  
-regtab1<-stargazer(m1rob,m2rob,m3rob,m4rob,m5rob,style="apsr",omit=c("coarse","chair","debate"),omit.stat=c("f","chi2"),
+regtab1<-stargazer(m1rob,m2rob,m3rob,m4rob,m5rob,style="apsr",omit=c("coarse","chair"),omit.stat=c("aic"),
                    dep.var.labels="Speaking time (seconds)",dep.var.labels.include=T,font.size="footnotesize",
-                   label="parlbias_regtab1",column.sep.width="5pt",star.cutoffs=c(.1,.05,.01),align=T,title="OLS models of speaking time",digits=2,
+                   label="parlbias_regtab1",column.sep.width="-5pt",star.cutoffs=c(.1,.05,.01),align=T,title="OLS models of speaking time",digits=2,
                    covariate.labels=covarlabs)
 
-regtab1<-c(regtab1[1:22],checkmarks,regtab1[23:length(regtab1)])
+regtab1<-c(regtab1[1:24],checkmarks,regtab1[25:length(regtab1)])
+regtab1
 writeLines(regtab1,con="../tables/parlbias_regtab1.txt")
 
-regtab1logit<-stargazer(m1.logit,m2.logit,m3.logit,m4.logit,m5.logit,style="apsr",omit=c("coarse","chair","debate"),omit.stat=c("f","chi2"),
+regtab1logit<-stargazer(m1.logit,m2.logit,m3.logit,m4.logit,m5.logit,style="apsr",omit=c("coarse","chair","debate"),omit.stat=c("wald"),
                         dep.var.labels="Dummy: speaking time exceeds 60 seconds",dep.var.labels.include=T,font.size="footnotesize",
-                        label="parlbias_regtab1logit",column.sep.width="5pt",covariate.labels=covarlabs,star.cutoffs=c(.1,.05,.01),
+                        label="parlbias_regtab1logit",column.sep.width="-5pt",covariate.labels=covarlabs,star.cutoffs=c(.1,.05,.01),
                         align=T,title="Logit models of exceeding standard speaking time",digits=2)
-
+regtab1logit
 regtab1logit<-c(regtab1logit[1:22],checkmarks,regtab1logit[23:length(regtab1logit)])
 writeLines(regtab1logit,con="../tables/parlbias_regtab1logit.txt")
 
 modscolumnlabels<-c("Full","Distance$\\leq$median (int.)","Distance$\\leq$median (ord.)","Same bloc")
 
-modscheckmarks<-c("Speaker party fixed effects & $\\checkmark$ & $\\checkmark$ & $\\checkmark$ & $\\checkmark$ \\\\", "Chair party fixed effects & $\\checkmark$ & $\\checkmark$ & $\\checkmark$ & $\\checkmark$ \\\\", "Debate fixed effects & $\\checkmark$ & $\\checkmark$ & $\\checkmark$ & $\\checkmark$ \\\\")
+modscheckmarks<-c("Speaker party FE & $\\checkmark$ & $\\checkmark$ & $\\checkmark$ & $\\checkmark$ \\\\", "Chair party FE & $\\checkmark$ & $\\checkmark$ & $\\checkmark$ & $\\checkmark$ \\\\")
+#, "Debate FE & $\\checkmark$ & $\\checkmark$ & $\\checkmark$ & $\\checkmark$ \\\\")
 
-regtabmods<-stargazer(m5,m5intposdifltmedian,m5ordposdifltmedian,m5cobloc,style="apsr",omit=c("coarse","chair","debate"),omit.stat=c("f","chi2","ser"),
+regtabmods<-stargazer(m5,m5intposdifltmedian,m5ordposdifltmedian,m5cobloc,style="apsr",omit=c("coarse","chair","debate"),omit.stat=c("wald"),
                       dep.var.labels="Speaking time (seconds)",dep.var.labels.include=T,font.size="footnotesize",
-                      label="parlbias_regtabmods",column.sep.width="0pt",covariate.labels=covarlabs,star.cutoffs=c(.1,.05,.01),
+                      label="parlbias_regtabmods",column.sep.width="-5pt",covariate.labels=covarlabs,star.cutoffs=c(.1,.05,.01),
                       align=T,title="Tests of political moderators",digits=2,column.labels=modscolumnlabels)
+
+regtabmods
 
 regtabmods<-c(regtabmods[1:23],modscheckmarks,regtabmods[24:length(regtabmods)])
 
@@ -167,6 +215,41 @@ modeffectests[4,1:2]<-tidy(m5cobloc)[2,2:3]
 modeffectests$model<-modeffectests$model %>%
   as.character() %>%
   gsub("\\$\\\\leq\\$","<=",.)
+
+#robustness check 1: restricted sample
+regtabrs<-stargazer(m1robrs,m2robrs,m3robrs,m4robrs,m5robrs,style="apsr",omit=c("coarse","chair"),omit.stat=c("wald"),
+                   dep.var.labels="Speaking time (seconds)",dep.var.labels.include=T,font.size="footnotesize",
+                   label="parlbias_regtabrs",column.sep.width="-5pt",star.cutoffs=c(.1,.05,.01),align=T,title="Results for only members of leadership parties",digits=2,
+                   covariate.labels=covarlabs)
+
+regtabrs<-c(regtabrs[1:24],checkmarks,regtabrs[25:length(regtabrs)])
+regtabrs
+writeLines(regtabrs,con="../tables/parlbias_regtabrs.txt")
+
+#robustness check 2: debate-specific fixed effects
+dfecheckmarks<-c("Speaker party FE & & & $\\checkmark$ & & $\\checkmark$ \\\\", "Chair party FE & & & & & $\\checkmark$ \\\\",
+                 "Debate FE & $\\checkmark$ & $\\checkmark$ & $\\checkmark$ & $\\checkmark$ & $\\checkmark$ \\\\")
+dfecovarlabs<-c("Copartisan","Time of day","Gender (female)","Intercept")
+
+regtabdfe<-stargazer(m1robdfe,m2robdfe,m3robdfe,m4robdfe,m5robdfe,style="apsr",omit=c("coarse","chair","debate"),omit.stat=c("wald"),
+                   dep.var.labels="Speaking time (seconds)",dep.var.labels.include=T,font.size="footnotesize",
+                   label="parlbias_regtabdfe",column.sep.width="-5pt",star.cutoffs=c(.1,.05,.01),align=T,title="Results with debate-specific fixed effects",digits=2,
+                   covariate.labels=dfecovarlabs)
+
+regtabdfe<-c(regtabdfe[1:22],dfecheckmarks,regtabdfe[23:length(regtabdfe)])
+regtabdfe
+writeLines(regtabdfe,con="../tables/parlbias_regtabdfe.txt")
+
+
+
+#table for reg predicting remarks presided over
+remarksregtab<-stargazer(remarksm1,remarksm2,remarksm3,style="apsr",omit.stat=c("wald"),
+                      dep.var.labels="Number of remarks enforced",dep.var.labels.include=T,font.size="footnotesize",
+                      label="parlbias_remarksregtab",column.sep.width="-5pt",star.cutoffs=c(.1,.05,.01),
+                      align=T,title="Model predicting number of remarks enforced by chairmen",digits=2,
+                      covariate.labels=c("Head chairman","No. of debates in leadership","No. of party seats"))
+
+writeLines(remarksregtab,con="../tables/parlbias_remarksregtab.txt")
 
 ### SUMMARY STATS TABLES
 ftall$type<-NA
@@ -235,12 +318,16 @@ ggplot(modeffectests,aes(x=est,y=reorder(model,c(4,3,2,1)))) +
 
 ggsave(file="../figures/parlbias_modeffectplot.pdf",height=4,width=9)
 
+#factors for copartisanship
 ft$copartisan_factor<-factor(ft$copartisan,labels=c("Non-copartisan","Co-partisan"))
 ft$copartisan_factor<-factor(ft$copartisan_factor,levels(ft$copartisan_factor)[c(2,1)])
 
-ggplot(subset(ft,chair==0 & !is.na(copartisan)),aes(x=secs)) +
-  geom_density(fill="gray",alpha=.5) +
-  facet_grid(.~copartisan_factor) +
+ft<-ft %>%
+  mutate(copartisancobloc=factor(copartisan+cobloc,labels=c("Non-copartisan, other bloc","Non-copartisan, same bloc","Co-partisan")))
+
+ggplot(subset(ft,chair==0 & !is.na(copartisancobloc)),aes(x=secs)) +
+  geom_density(fill="gray",alpha=.5,adjust=1.5) +
+  facet_grid(.~copartisancobloc) +
   geom_vline(xintercept=60,linetype="dashed") +
   xlab("Speech duration (seconds)") +
   ylab("") +
@@ -253,8 +340,6 @@ ggplot(subset(ft,chair==0 & !is.na(copartisan)),aes(x=secs)) +
         panel.border = element_rect(colour = "black"))
 
 ggsave(file="../figures/parlbias_dens.pdf",height=5,width=9)
-
-plot(density(ft$timeofday))
 
 ## varying coefficients by party
 
@@ -293,6 +378,21 @@ ggplot(chairranefs,aes(x=coef,y=reorder(chairparty,-chairorder))) +
         panel.border = element_rect(colour = "black"))
 
 ggsave(file="../figures/parlbias_chairranefs.pdf",height=6,width=9)
+
+
+## plot predicted remarks presided over
+seatsprdf1<-data.frame(expand.grid(debates=1:9,president=0:1,avgseats=21)) %>%
+  bind_cols(.,as.data.frame(predict(remarksm2,newdata=.,se.fit=T))) %>%
+  dplyr::select(debates,president,fit,se.fit)
+
+
+ggplot(allchairmen,aes(debates,remarks)) + geom_point() + geom_smooth(method="lm")
+
+ggplot(allchairmen,aes(avgseats,remarks)) + geom_point() + geom_smooth(method="lm")
+
+median(allchairmen$avgseats)
+median(allchairmen$debates)
+
 
 ### PLOTS
 
--- manuscript/revisionnotes.md
+++ manuscript/revisionnotes.md
@@ -12,43 +12,59 @@ Thank you for giving me the opportunity to revise my manuscript "Intergroup Bias
 
 Below I address the specific comments made by each of the reviewers in turn. These sections are labeled 1.1 for R1's first comment, 1.2 for R1's second comment, and so on.
 
+In response to several of the reviewers' comments, I have added new figures and tables to the manuscript, some in the main text, some in the appendix. For ease of reading, I reproduce these tables and figures below, along with other material only shown in this note.
+
 ## Reviewer 1
 
 ### 1.1: Data to justify 'as if' randomness
 
-graph showing that chairperson time is allocated proportionally to seats (explain why this is not the case)
+R1 requests more empirical data in support of the claim that assignment of chairpersons to debates is "as if" random. Specifically, R1 asks for a graph showing the percentage of seats controlled by the chairperson on the x-axis and the percentage of times chairing a debate on the y-axis, noting that the points in such a graph should fall along a 45 degree line.
+
+I thank R1 for this suggestion. However, I do not believe the predicted pattern would constitute evidence in support of "as if" random assignment. The reason for this is that members of the leadership (with some exceptions for the head chairperson) are treated equally, with no consideration given to their party's share of seats in parliament. Hence, a chairperson from the 5th largest party has the same obligations as the chairperson from the largest party (again, with some exceptions for the head chairperson). Seat share should thus not be a predictor of chairperson activity. I want to stress that the manuscript was not sufficiently clear on this point, and I have added a paragraph on p. XXX making this point more explicitly.
+
+As additional evidence for the "as if" random assignment of chairpersons to debates, I have added section A.1, "Predicting chairman activity", to the appendix. Here I present results from regression models predicting how many debates chairpersons control. The key result from these models is that the main predictor of number of debates controlled is how long a chairperson has been a member of the leadership. There is an additional small premium for being the head chairperson, which likely reflects the fact that by convention, the president always takes the first shift controlling a debate. In contrast, number of seats has no significant association. The results are shown below.
+
+\input{../tables/parlbias_remarksregtab.txt}
+
+The results complement the qualitative evidence from correspondence with the parliament leadership that chairperson activity is largely driven by 'supply-side' factors, i.e. chairpersons' availability on the day of the debate, and not by party specific factors such as their parliamentary power.
 
 ### 1.2: Abrupt conclusion
 
-Add in discussion about all of these things:
+R1 notes that the conclusion is abrupt and should include more discussion of the implications of the finding.
 
-What does it imply about how legislative rules and institutions are formed?
+I have extended the conclusion section with more discussion of the findings's implications. For the specific wording of the discussion, I refer to the manuscript. In short, I argue that the findings imply that intergroup bias can occur even in institutional and cultural settings that should work strongly against bias (the same reasons for which I argue the setting is a 'least likely' case). At the same time, the observed effect is fairly small, suggesting that the aforementioned factors do go some way towards mitigating bias. Lastly, and perhaps most importantly, they illustrate the importance of balancing institutions. As added to the discussion in section 5.2, the effects on speaking time for each chairperson roughly wash out within each debate due to the rotation of chairpersons. This does not nullify the finding per se, but illustrates how the principle of rotating chairperson roles can result in (approximate) equity in the aggregate even in the presence of bias at the individual level (see also the discussion in section 3.3 below). I think this added discussion has strenghtened the manuscript, and I thank R1 for the suggestion.
+
+<!-- What does it imply about how legislative rules and institutions are formed?
 
 What are plausible things that might mitigate this bias?
 
-Does the answer to the question depend on the mechanism? If so, how?
+Does the answer to the question depend on the mechanism? If so, how? -->
 
 ### 1.3: Minor points
 
-'modal' in footnote 1
-
-section 5.2 seems unnecessary
+Lastly, R1 mentions some minor points. I have fixed the word in footnote 1 to 'modal', as was indeed intended. R1 also notes that section 5.2 seems unnecessary, which I agree with in principle. However, section 5.2, which discusses the magnitude of the effect, speaks to a criticism sometimes raised against the finding: namely, that the observed effect is fairly small. This is indeed the case, and in section 5.2, I make an effort to be explicit about this point. Hence, I have opted to keep section 5.2, though I agree that it could be omitted if need be.
 
 ## Reviewer 2
 
 ### 2.1: Data to justify 'as if' randomness
 
-Concern that chairmen self-select into issues for which co-partisans speak longer
+R2 also suggests additional evidence justifying the assumption of "as if" randomness. Specifically, R2 expresses concern that chairmen might self-select into particular issues of high importance to the party. If party members also take more time discussing these issues, this self-selection would produce a spurious finding of own-group bias. As a way of countering this concern, R2 suggests showing that the issues debated are similar in copartisan and non-copartisan matches.
 
-FH: this is a pretty clever idea! need to show empirically
+This is indeed a relevant concern, and I thank R2 for pointing it out. [HVORFOR MAN IKKE KAN MÅLE INDHOLD]
 
-need to somehow show that the content of the debate is the same for copartisan and non-copartisan
+However, two aspects of how debates are organized indicate that issue-based self-selection is not likely. First of all, opening or closing debates are not about any one issue, but cover a multitude of issues, mostly based on issue disagreements between parties. MP's may debate one issue for a few remarks, and then shift to another issue. Second of all, the schedule of which chairmen oversee the debate is set in advance of the debate. Hence, chairmen are not able to opt in to the role when a particular issue comes up in the debate. The combination of these two aspects suggests self-selection based on issues is unlikely. I have added a paragraph in section 3.1 of the manuscript making this point explicitly.
+
+For additional evidence in favor of the "as if" randomness assumption, see section 1.1 above.
 
 ### 2.2: Debate-specific fixed effects
 
-would allow for holding issue constant (FH: not really)
+As additional evidence in favor of the "as if" randomness assumption, R2 suggests employing debate-specific fixed effects. As mentioned, each debate covers several issues, so debate fixed effects should not be interpreted as holding issue content constant. Nevertheless, debate-specific fixed effects is a valuable robustness check of the results. Table 7 in section A.2 in the appendix shows the main results with debate-specific fixed effects added in every specification. The results are robust to including debate-specific fixed effects. The table is also shown below.
+
+\input{../tables/parlbias_regtabdfe.txt}
 
 ### 2.3: More information about speaking assignment rules
+
+TJEK OM DEBATSYSTEMET ER ELEKTRONISK
 
 add in some stuff from forretningsordenen
 
@@ -58,6 +74,8 @@ do chairmen call on more people from his/her own party? add discussion about thi
 
 I really don't know how to handle this
 
+The pattern suggests some of the difference reflects self-censorship.
+
 ## Reviewer 3
 
 ### 3.1: Partisanship as a 'soft' test
@@ -65,6 +83,8 @@ I really don't know how to handle this
 explain why minimal groups wouldn't just be better
 
 discuss this some more, hard relative to other tests, partisanship is not the same as in the US
+
+in the realm of partisan identities, this test is relatively hard
 
 ### 3.2: Pattern of queues of brief remarks
 
